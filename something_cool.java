2fc2dd3698c3c371658b65a999bf20e758f9521b
supermannen420 <eltram06@gmail.com>
2023-10-05 14:25:38 +0200

    jnj

---
+++ something_cool.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class something_cool {
+
+    public static void main(String[] args) {
+        System.out.println("kryptera_ord_skriv_1");
+        System.out.println("lös_kryptering_skriv_2");
+
+        Scanner kryptera = new Scanner(System.in);
+        String answer = kryptera.nextLine();
+
+       int nummber = Integer.parseInt(answer);
+
+        if (nummber == 1){
+            System.out.println("kryptera");
+
+            System.out.println("skriv de som ska krypteras");
+            Scanner OrdIn = new Scanner(System.in);
+            String ord = OrdIn.nextLine();
+           
+
+
+
+
+        }else if (nummber == 2){
+            System.out.println("lös");
+        }else{
+            System.exit(0);
+        }
+
+
+
+        // om 1 kryptera om 2 lös
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
